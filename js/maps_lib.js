3f4d255e47bab277a824ac73f95d84dcf591135e
Aaron Politsky <aaron.politsky@gmail.com>
2015-05-26 13:58:40 -0500

    tidies specialty checkbox whereClause

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -190,20 +190,20 @@
 
         //------Specific Service checkboxes---------
         
-        if ( $("#cbType1").is(':checked')) self.whereClause  += " AND " + (" 'Recovery / Halfway Homes'='Yes'");
-        if ( $("#cbType2").is(':checked')) self.whereClause  += " AND " + (" 'Cognitive Behavioral Therapy'='Yes'");
-        if ( $("#cbType3").is(':checked')) self.whereClause  += " AND " + (" 'Medication-Assisted Treatment'='Yes'");
-        if ( $("#cbType4").is(':checked')) self.whereClause  += " AND " + (" 'Mental Illness and Substance Abuse (MISA)'='Yes'");
-        if ( $("#cbType5").is(':checked')) self.whereClause  += " AND " + (" 'Veteran-specific'='Yes'");
-        if ( $("#cbType6").is(':checked')) self.whereClause  += " AND " + (" 'Trauma-informed'='Yes'");
-        if ( $("#cbType7").is(':checked')) self.whereClause  += " AND " + (" 'Individual counseling'='Yes'");
-        if ( $("#cbType8").is(':checked')) self.whereClause  += " AND " + (" 'Community meetings'='Yes'");
-        if ( $("#cbType9").is(':checked')) self.whereClause  += " AND " + (" 'Domestic Violence Treatment'='Yes'");
-        if ( $("#cbType10").is(':checked')) self.whereClause += " AND " + (" 'Anger Management'='Yes'");
-        if ( $("#cbType11").is(':checked')) self.whereClause += " AND " + (" 'Parenting Classes'='Yes'");
-        if ( $("#cbType12").is(':checked')) self.whereClause += " AND " + (" 'Treatment Readiness Groups'='Yes'");
-        if ( $("#cbType13").is(':checked')) self.whereClause += " AND " + (" 'Drunk Driving'='Yes'");
-        if ( $("#cbType14").is(':checked')) self.whereClause += " AND " + (" 'Adolescent'='Yes'");          
+        if ( $("#cbType1").is(':checked')) self.whereClause  += " AND 'Recovery / Halfway Homes'='Yes'";
+        if ( $("#cbType2").is(':checked')) self.whereClause  += " AND 'Cognitive Behavioral Therapy'='Yes'";
+        if ( $("#cbType3").is(':checked')) self.whereClause  += " AND 'Medication-Assisted Treatment'='Yes'";
+        if ( $("#cbType4").is(':checked')) self.whereClause  += " AND 'Mental Illness and Substance Abuse (MISA)'='Yes'";
+        if ( $("#cbType5").is(':checked')) self.whereClause  += " AND 'Veteran-specific'='Yes'";
+        if ( $("#cbType6").is(':checked')) self.whereClause  += " AND 'Trauma-informed'='Yes'";
+        if ( $("#cbType7").is(':checked')) self.whereClause  += " AND 'Individual counseling'='Yes'";
+        if ( $("#cbType8").is(':checked')) self.whereClause  += " AND 'Community meetings'='Yes'";
+        if ( $("#cbType9").is(':checked')) self.whereClause  += " AND 'Domestic Violence Treatment'='Yes'";
+        if ( $("#cbType10").is(':checked')) self.whereClause += " AND 'Anger Management'='Yes'";
+        if ( $("#cbType11").is(':checked')) self.whereClause += " AND 'Parenting Classes'='Yes'";
+        if ( $("#cbType12").is(':checked')) self.whereClause += " AND 'Treatment Readiness Groups'='Yes'";
+        if ( $("#cbType13").is(':checked')) self.whereClause += " AND 'Drunk Driving'='Yes'";
+        if ( $("#cbType14").is(':checked')) self.whereClause += " AND 'Adolescent'='Yes'";          
 
 
         //-----end of custom filters-----
